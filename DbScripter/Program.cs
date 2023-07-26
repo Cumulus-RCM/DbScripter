7e37cec7119008fa7c7579916c897cb24718a77d
stokara <avistokar@gmail.com>
2023-07-26 09:12:00 -0400

    Add project files.

---
+++ DbScripter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DbScripter;
+
+public class Program {
+    static async Task Main(string[] args) {
+        const string DB_NAME = "Billing2"; // database name  
+
+        const string LOCAL = "(localdb)\\MSSQLLocalDB";
+
+        var azureConnString =
+            "Server=tcp:cumulus-dev-db.database.windows.net,1433;Integrated Security=false;Authentication=Active Directory Interactive";
+
+        const bool IS_LOCAL = true;
+        var connString = IS_LOCAL ? LOCAL : azureConnString;
+
+        var filename = @"c:\temp\db.json";
+        var fileStream = new FileStream(filename, FileMode.Create);
+        await ScriptWriter.GetScriptAsync(connString, IS_LOCAL, DB_NAME, fileStream);
+        fileStream.Close();
+        fileStream.Dispose();
+
+        await ScriptWriter.CreatePrettyFileAsync(filename);
+    }
+}
+    
---
+++ DbScripter/ScriptWriter.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
+using System.Text.Json;
+
+namespace DbScripter;
+
+public class ScriptWriter {
+    private record sqlJson(string ScriptValue = "");
+
+    public static async Task GetScriptAsync(string connString, bool isLocal, string databaseName, Stream stream) {
+       
+        var sqlStrings = generateScriptsToStrings(databaseName, isLocal, connString);
+        await writeJsonAsync(sqlStrings, stream).ConfigureAwait(false);
+
+        //-----------------------------------------------------------------------------------
+        static string[] generateScriptsToStrings(string databaseName, bool isLocal, string connString) {
+            var scriptingOptions = new ScriptingOptions {
+                AllowSystemObjects = false,
+                ScriptOwner = false,
+                NoCollation = true,
+                IncludeIfNotExists = false,
+                ScriptDrops = false,
+                WithDependencies = true,
+                Indexes = true,
+                Triggers = true,
+                DriForeignKeys = true,
+                DriAllConstraints = true
+            };
+            var server = createServer(isLocal, connString);
+            var header = $"--Server Version:{server.Information.Version} Generated:{DateTime.Now}";
+            Console.WriteLine(header);
+
+            var database = server.Databases[databaseName];
+            var generatedSql = new string[3];
+            var scripter =  new Scripter(server) { Options = scriptingOptions };
+
+            var tables = database.Tables.Cast<Table>()
+                .Where(tb => !tb.IsSystemObject && tb.Name != "AppliedMigrationScript")
+                .Select(t => t.Urn)
+                .ToArray();
+            generatedSql[0] = getSqlForDbObjects(tables, scripter);
+
+            var functions = database.UserDefinedFunctions.Cast<UserDefinedFunction>()
+                .Where(x => !x.IsSystemObject)
+                .Select(t => t.Urn)
+                .ToArray();
+            scripter.Options.WithDependencies = false;
+            generatedSql[1] = getSqlForDbObjects(functions, scripter);
+
+            var procedures = database.StoredProcedures.Cast<StoredProcedure>()
+                .Where(x => !x.IsSystemObject)
+                .Select(x => x.Urn)
+                .ToArray();
+            generatedSql[2] = getSqlForDbObjects(procedures, scripter);
+
+            return generatedSql;
+        }
+
+        static Server createServer(bool isLocal, string connString) {
+            if (isLocal) return new Server(connString);
+
+            using var conn = new SqlConnection(connString);
+            conn.Open();
+            return new Server(new ServerConnection(conn));
+        }
+
+        static string getSqlForDbObjects(Urn[] urns, Scripter scripter) {
+            var script = scripter.Script(urns);
+            var sb = new StringBuilder();
+            foreach (var line in script) {
+                if (line is null || line.StartsWith("SET")) continue;
+                sb.AppendLine(line);
+            }
+            return sb.ToString();
+        }
+
+        static async Task writeJsonAsync(IEnumerable<string> sqlStrings, Stream stream) {
+            var json = JsonSerializer.Serialize(sqlStrings.Select(s => new sqlJson(s)));
+            await using var streamWriter = new StreamWriter(stream);
+            await streamWriter.WriteAsync(json).ConfigureAwait(false);
+        }
+    }
+
+    
+    public static async Task CreatePrettyFileAsync(string filename) {
+        var stream = File.Open(filename, FileMode.Open);
+        var json = await JsonDocument.ParseAsync(stream);
+        var pretty = json.Deserialize<IEnumerable<sqlJson>>(new JsonSerializerOptions { WriteIndented = true });
+        await using var prettyStream = File.CreateText(filename + ".pretty");
+        foreach (var s in pretty) {
+            await prettyStream.WriteAsync(s.ScriptValue);
+        }
+    }
+}
